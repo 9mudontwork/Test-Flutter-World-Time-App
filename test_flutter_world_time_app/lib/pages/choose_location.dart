de24999051b1d3a574c732d64c2100526aab11e0
Muhammad Imoeb <muhammad@diversition.co.th>
2020-09-27 02:00:55 +0700

    ทดสอบ Flutter Widget Lifecycle

--- test_flutter_world_time_app/lib/pages/choose_location.dart
+++ test_flutter_world_time_app/lib/pages/choose_location.dart
@@ -6,10 +6,33 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState function run');
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('Build function run');
     return Scaffold(
-      body: Text('choose location screen'),
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a Location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: RaisedButton(
+        onPressed: () {
+          setState(() {
+            counter += 1;
+          });
+        },
+        child: Text('counter is $counter'),
+      ),
     );
   }
 }
