b0f805923c7481584a8390faacfccb6ee3b01a3b
Muhammad Imoeb <muhammad@diversition.co.th>
2020-09-27 03:57:52 +0700

    ทดสอบการเรียก api และการจัดการข้อมูลที่ response จาก api

--- test_flutter_world_time_app/lib/pages/loading.dart
+++ test_flutter_world_time_app/lib/pages/loading.dart
@@ -9,18 +9,26 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
+  void getTime() async {
     Response response =
-        await get('https://jsonplaceholder.typicode.com/todos/1');
+        await get('http://worldtimeapi.org/api/timezone/Asia/Bangkok');
     Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+    // print(data);
+
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+    // print(datetime);
+    // print(offset);
+
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
