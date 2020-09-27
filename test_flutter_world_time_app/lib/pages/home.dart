5b885ac99acf4614a5f3251924e8025793465639
Muhammad Imoeb <muhammad@diversition.co.th>
2020-09-28 00:40:20 +0700

    ทดสอบใช้ package ช่วย format วันที่ และ โชวันที่

--- test_flutter_world_time_app/lib/pages/home.dart
+++ test_flutter_world_time_app/lib/pages/home.dart
@@ -15,16 +15,39 @@ class _HomeState extends State<Home> {
 
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            FlatButton.icon(
-              onPressed: () {
-                Navigator.pushNamed(context, '/location');
-              },
-              icon: Icon(Icons.edit_location),
-              label: Text('Edit Location'),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+          child: Column(
+            children: [
+              FlatButton.icon(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/location');
+                },
+                icon: Icon(Icons.edit_location),
+                label: Text('Edit Location'),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    data['location'],
+                    style: TextStyle(
+                      fontSize: 20,
+                      letterSpacing: 2,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Text(
+                data['time'],
+                style: TextStyle(
+                  fontSize: 66,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
