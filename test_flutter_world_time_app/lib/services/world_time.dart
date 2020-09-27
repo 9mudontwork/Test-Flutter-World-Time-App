fdda6c05b05fbf916613a7c6adb976a882ccefb0
Muhammad Imoeb <muhammad@diversition.co.th>
2020-09-27 23:41:06 +0700

    ตัวอย่างการจัดการ error

--- test_flutter_world_time_app/lib/services/world_time.dart
+++ test_flutter_world_time_app/lib/services/world_time.dart
@@ -10,19 +10,25 @@ class WorldTime {
   WorldTime({this.location, this.flag, this.url});
 
   Future<void> getTime() async {
-    ///Asia/Bangkok
-    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
-    Map data = jsonDecode(response.body);
-    // print(data);
+    try {
+      ///Asia/Bangkok
+      Response response =
+          await get('http://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body);
+      // print(data);
 
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-    // print(datetime);
-    // print(offset);
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
+      // print(datetime);
+      // print(offset);
 
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
 
-    time = now.toString();
+      time = now.toString();
+    } catch (e) {
+      print('caught error: $e');
+      time = 'cloud not get time data';
+    }
   }
 }
